9ab64a2c68caa9524041d017e153b271c437a2af
KeKs0r <marc.hoeffl@laserfocus.io>
2022-09-28 16:26:57 +0200

    Build Example Middleware

---
+++ packages/orchid/src/__tests__/example-middleware.ts
@@ -0,0 +1,30 @@
+import { Middleware } from '../middleware.types';
+import { TaskSpec } from '../task.types';
+
+interface ExampleMiddlewareOptions {
+  onPreTask?: (task: TaskSpec<any, any>, input: any) => void;
+  onContext?: <T>(ctx: T) => T;
+  onPostTask?: (task: TaskSpec<any, any>, result: any) => void;
+}
+export function makeExampleMiddleware(
+  options: ExampleMiddlewareOptions
+): Middleware {
+  const exampleMiddleware: Middleware = async function exampleMiddleware(
+    task,
+    input,
+    ctx,
+    next
+  ) {
+    if (options.onPreTask) {
+      options.onPreTask(task, input);
+    }
+    const context = options.onContext ? options.onContext(ctx) : ctx;
+    const result = await next(input, context);
+    if (options.onPostTask) {
+      options.onPostTask(task, result);
+    }
+    return result;
+  };
+
+  return exampleMiddleware;
+}
