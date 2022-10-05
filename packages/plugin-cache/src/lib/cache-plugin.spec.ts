c7883c5c55acd4124d32dd8cd3dbab478f1779fb
Marc Höffl <marc.hoeffl@gmail.com>
2022-10-06 01:55:54 +0200

    Fix cacheplugin access issue (#14)
    * Fix cacheplugin access issue
    
    * Add lint command to package
    
    * Use all tests

--- packages/plugin-cache/src/lib/cache-plugin.spec.ts
+++ packages/plugin-cache/src/lib/cache-plugin.spec.ts
@@ -10,7 +10,7 @@ describe('Plugin Cache', () => {
     const spy = vitest.fn((a) => a * 2);
     const task = makeTask(spy, {
       getCacheKey(input) {
-        return `input:${input}`;
+        return `double:${input}`;
       },
     });
 
@@ -26,13 +26,14 @@ describe('Plugin Cache', () => {
   });
 
   it('Can invalidate cache', async () => {
-    const { app } = getApp();
     const spy = vitest.fn((a) => a);
     const task = makeTask(spy, {
       getCacheKey(input) {
         return `input:${input}`;
       },
     });
+
+    const { app } = getApp();
     await app.run(task, 3);
     await app.run(task, 3);
     expect(spy).toHaveBeenCalledOnce();
@@ -49,8 +50,63 @@ describe('Plugin Cache', () => {
     expect(spy).toHaveBeenCalledTimes(2);
   });
 
-  it.todo('Migrate cache (+ check meta after)');
-  it.todo('Cache meta not upgraded if result is the same');
+  it('Migrate cache (+ check meta after)', async () => {
+    const spy = vitest.fn((a) => a);
+    const task = makeTask(spy, {
+      getCacheKey(input) {
+        return `input:${input}`;
+      },
+      meta: {
+        version: 1,
+      },
+    });
+
+    const { app, storage } = getApp();
+    await app.run(task, 3);
+    await app.run(task, 4);
+    await app.run(task, 5);
+
+    const taskV2 = makeTask(spy, {
+      getCacheKey(input) {
+        return `input:${input}`;
+      },
+      onPostLoad(item) {
+        // Invalidate
+        if (item.result === 4) {
+          return null;
+        }
+        if (item.result === 5) {
+          // migrate
+          return 12;
+        }
+        return item.result;
+      },
+      meta: {
+        version: 2,
+      },
+    });
+
+    await app.run(taskV2, 3);
+    await app.run(taskV2, 4);
+    await app.run(taskV2, 5);
+
+    const data = storage.getData();
+
+    expect(data).toHaveProperty('input:3');
+    const three = data['input:3'];
+    expect(three).toHaveProperty('result', 3);
+    expect(three).toHaveProperty('meta.version', 1);
+
+    expect(data).toHaveProperty('input:4');
+    const four = data['input:4'];
+    expect(four).toHaveProperty('result', 4);
+    expect(four).toHaveProperty('meta.version', 2);
+
+    expect(data).toHaveProperty('input:5');
+    const five = data['input:5'];
+    expect(five).toHaveProperty('result', 12);
+    expect(five).toHaveProperty('meta.version', 2);
+  });
 });
 
 function getApp() {
